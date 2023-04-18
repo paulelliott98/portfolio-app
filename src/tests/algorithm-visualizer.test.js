87242723c68d6fde5469dbd3d510639419e6c783
Paul Gan <paulgan98@gmail.com>
2023-04-18 12:33:45 -0700

    Update test cases

--- src/tests/algorithm-visualizer.test.js
+++ src/tests/algorithm-visualizer.test.js
@@ -1,105 +1,111 @@
+const utils = require("../utils");
 const blocks = require("../components/algorithm-visualizer/blocks");
 const mg = require("../components/algorithm-visualizer/mazeGenerator");
-const utils = require("../utils");
 
 const dirs = { up: 0, right: 1, down: 2, left: 3 };
 
-const [nRows, nCols] = [64, 64];
+describe("Maze Generator Tests", () => {
+  var nRows, nCols;
+  var newMaze;
 
-test("randInt should pick random int between [a, b] inclusive", () => {
-  let s = new Set();
-  for (let i = 0; i < 200; i++) {
-    s.add(utils.randInt(0, 3));
-  }
-  expect(s.size).toBe(4); // 0,1,2,3
-});
+  beforeAll(() => {
+    [nRows, nCols] = [64, 64];
+  });
 
-test("randChoice should choose be able to pick any item in list", () => {
-  let s = new Set();
-  const d = ["up", "right", "down", "left"];
-  for (let i = 0; i < 200; i++) {
-    let item = utils.randChoice(d);
-    s.add(item);
-  }
-  expect(s.size).toBe(d.length);
-  expect(s.has("up")).toBe(true);
-  expect(s.has("down")).toBe(true);
-  expect(s.has("left")).toBe(true);
-  expect(s.has("right")).toBe(true);
-});
+  // beforeEach(() => {
+  //   newMaze = null;
+  // });
 
-test("getSideBlocks", () => {
-  let newMaze = mg.generateMaze(nRows, nCols).grid;
-  newMaze[5][4] = -1; // right
-  newMaze[5][6] = -1; // left
-  newMaze[4][5] = -2; // up
-  newMaze[6][5] = -2; // down
+  test("getSideBlocks", () => {
+    newMaze = utils.createArray(nRows, nCols);
+    newMaze[5][4] = -1; // right
+    newMaze[5][6] = -1; // left
+    newMaze[4][5] = -2; // up
+    newMaze[6][5] = -2; // down
 
-  let leftRight = mg.getSideBlocks({ r: 5, c: 5 }, dirs.down, newMaze);
-  let upDown = mg.getSideBlocks({ r: 5, c: 5 }, dirs.right, newMaze);
+    let leftRight = mg.getSideBlocks({ r: 5, c: 5 }, dirs.down, newMaze);
+    let upDown = mg.getSideBlocks({ r: 5, c: 5 }, dirs.right, newMaze);
 
-  expect(leftRight[0]).toBe(-1); // left
-  expect(leftRight[1]).toBe(-1); // right
-  expect(upDown[0]).toBe(-2); // up
-  expect(upDown[1]).toBe(-2); // down
-});
+    expect(leftRight[0]).toBe(-1); // left
+    expect(leftRight[1]).toBe(-1); // right
+    expect(upDown[0]).toBe(-2); // up
+    expect(upDown[1]).toBe(-2); // down
+  });
 
-test("isWall", () => {
-  let newMaze = mg.generateMaze(nRows, nCols).grid;
+  test("isWall", () => {
+    newMaze = utils.createArray(nRows, nCols, blocks.wall);
 
-  let wall = { r: 0, c: 1 };
-  expect(mg.isWall(wall, dirs.down, newMaze)).toBe(true);
+    expect(mg.isWall({ r: 0, c: 1 }, dirs.down, newMaze)).toBe(true);
+    expect(mg.isWall({ r: nRows - 1, c: 1 }, dirs.up, newMaze)).toBe(true);
+    expect(mg.isWall({ r: 1, c: 0 }, dirs.right, newMaze)).toBe(true);
+    expect(mg.isWall({ r: 1, c: nCols - 1 }, dirs.left, newMaze)).toBe(true);
 
-  wall = { r: nRows - 1, c: 1 };
-  expect(mg.isWall(wall, dirs.up, newMaze)).toBe(true);
+    newMaze[3][3] = blocks.empty;
+    expect(mg.isWall({ r: 2, c: 2 }, dirs.down, newMaze)).toBe(true);
+    expect(mg.isWall({ r: 4, c: 4 }, dirs.left, newMaze)).toBe(true);
+    expect(mg.isWall({ r: 4, c: 2 }, dirs.right, newMaze)).toBe(true);
+    expect(mg.isWall({ r: 4, c: 3 }, dirs.up, newMaze)).toBe(true);
 
-  wall = { r: 1, c: 0 };
-  expect(mg.isWall(wall, dirs.right, newMaze)).toBe(true);
+    expect(mg.isWall({ r: 10, c: 10 }, dirs.up, newMaze)).toBe(false);
+    expect(mg.isWall({ r: 10, c: 10 }, dirs.down, newMaze)).toBe(false);
+    expect(mg.isWall({ r: 10, c: 10 }, dirs.left, newMaze)).toBe(false);
+    expect(mg.isWall({ r: 10, c: 10 }, dirs.right, newMaze)).toBe(false);
+  });
 
-  wall = { r: 1, c: nCols - 1 };
-  expect(mg.isWall(wall, dirs.left, newMaze)).toBe(true);
-
-  newMaze[3][3] = blocks.empty;
-  expect(mg.isWall({ r: 2, c: 2 }, dirs.down, newMaze)).toBe(true);
-  expect(mg.isWall({ r: 4, c: 4 }, dirs.left, newMaze)).toBe(true);
-  expect(mg.isWall({ r: 4, c: 2 }, dirs.right, newMaze)).toBe(true);
-  expect(mg.isWall({ r: 4, c: 3 }, dirs.up, newMaze)).toBe(true);
-
-  expect(mg.isWall({ r: 10, c: 10 }, dirs.up, newMaze)).toBe(false);
-  expect(mg.isWall({ r: 10, c: 10 }, dirs.down, newMaze)).toBe(false);
-  expect(mg.isWall({ r: 10, c: 10 }, dirs.left, newMaze)).toBe(false);
-  expect(mg.isWall({ r: 10, c: 10 }, dirs.right, newMaze)).toBe(false);
-});
-
-test("initStartPos should return coord on border of grid", () => {
-  for (let i = 0; i < 4; i++) {
-    for (let k = 0; k < nRows * 3; k++) {
-      const startPos = mg.initStartPos(i, nRows, nCols);
-      if (i === dirs.up) expect(startPos.r).toBe(nRows - 1);
-      else if (i === dirs.down) expect(startPos.r).toBe(0);
-      else if (i === dirs.left) expect(startPos.c).toBe(nCols - 1);
-      else if (i === dirs.right) expect(startPos.c).toBe(0);
+  test("initStartPos should return coord on border of grid", () => {
+    for (let i = 0; i < 4; i++) {
+      for (let k = 0; k < nRows * 3; k++) {
+        const startPos = mg.initStartPos(i, nRows, nCols);
+        if (i === dirs.up) expect(startPos.r).toBe(nRows - 1);
+        else if (i === dirs.down) expect(startPos.r).toBe(0);
+        else if (i === dirs.left) expect(startPos.c).toBe(nCols - 1);
+        else if (i === dirs.right) expect(startPos.c).toBe(0);
+      }
     }
-  }
+  });
+
+  test("getTurnDirection", () => {
+    const rightTurns = [
+      [dirs.up, dirs.right],
+      [dirs.right, dirs.down],
+      [dirs.down, dirs.left],
+      [dirs.left, dirs.up],
+    ];
+
+    for (let i in rightTurns) {
+      const [prev, next] = rightTurns[i];
+      expect(mg.getTurnDirection(prev, next)).toBe(dirs.right);
+      expect(mg.getTurnDirection(next, prev)).toBe(dirs.left);
+    }
+  });
+
+  // test("generateMaze: maze should be nRows x nCols", () => {
+  //   newMaze = mg.generateMaze(63, 63).grid;
+  //   expect(newMaze.length).toBe(63);
+  //   expect(newMaze[0].length).toBe(63);
+  // });
 });
 
-test("getTurnDirection", () => {
-  const rightTurns = [
-    [dirs.up, dirs.right],
-    [dirs.right, dirs.down],
-    [dirs.down, dirs.left],
-    [dirs.left, dirs.up],
-  ];
+describe("Util Functions Tests", () => {
+  test("randInt should pick random int between [a, b] inclusive", () => {
+    let s = new Set();
+    for (let i = 0; i < 200; i++) {
+      s.add(utils.randInt(0, 3));
+    }
+    expect(s.size).toBe(4); // 0,1,2,3
+  });
 
-  for (let i in rightTurns) {
-    const [prev, next] = rightTurns[i];
-    expect(mg.getTurnDirection(prev, next)).toBe(dirs.right);
-    expect(mg.getTurnDirection(next, prev)).toBe(dirs.left);
-  }
-});
-
-test("generateMaze: maze should be nRows x nCols", () => {
-  let maze = mg.generateMaze(63, 63).grid;
-  expect(maze.length).toBe(63);
-  expect(maze[0].length).toBe(63);
+  test("randChoice should choose be able to pick any item in list", () => {
+    let s = new Set();
+    const d = ["up", "right", "down", "left"];
+    for (let i = 0; i < 200; i++) {
+      let item = utils.randChoice(d);
+      s.add(item);
+    }
+    expect(s.size).toBe(d.length);
+    expect(s.has("up")).toBe(true);
+    expect(s.has("down")).toBe(true);
+    expect(s.has("left")).toBe(true);
+    expect(s.has("right")).toBe(true);
+  });
 });
