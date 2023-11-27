516e98a2bcc5e217d9e3178e1d8179e18551e52d
Paul Gan <paulgan98@gmail.com>
2023-11-26 23:40:45 -0800

    Styling changes

---
+++ src/components/sorting-visualizer/SortingVisualizer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+
+const SortingVisualizer = ({}) => {
+  return (
+    <Grid item container style={{ flexFlow: 'row nowrap' }}>
+      <Grid item container style={{}}></Grid>
+    </Grid>
+  );
+};
+
+export default SortingVisualizer;
